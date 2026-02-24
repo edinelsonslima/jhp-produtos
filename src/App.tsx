fa8bc332406d1c49736f45739374991f0616ca46
Edinelson Lima <edinelsonlima86@gmail.com>
2026-02-24 01:37:35 -0300

    refactor: restructure routing logic using createBrowserRouter and createRoutesFromElements for improved code organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,96 @@
 import { Toaster } from "@/components/_ui/toast";
 import { authStore } from "@/hooks/useAuth";
 import { domMax, LazyMotion } from "framer-motion";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import AppLayout from "./components/_layout";
-import About from "./pages/About";
-import Audit from "./pages/Audit";
-import EditSale from "./pages/EditSale";
-import History from "./pages/History";
-import Index from "./pages/Index";
-import Login from "./pages/Login";
-import Payments from "./pages/Payments";
-import Products from "./pages/Products";
-import Sales from "./pages/Sales";
-import Search from "./pages/Search";
+import { Authenticated } from "./components/auth/authenticated";
+import { Unauthenticated } from "./components/auth/unauthenticated";
 
 function AuthGate() {
   const user = authStore.useStore((state) => state.user);
+  const logged = user !== null;
 
-  if (!user) {
-    return <Login />;
-  }
+  return createRoutesFromElements(
+    <Route errorElement={<div>Ops... Página não encontrada</div>}>
+      <Route element={<Unauthenticated logged={logged} />}>
+        <Route
+          path="*"
+          lazy={() => import("@/pages/Login")}
+          hydrateFallbackElement={<div>carregando...</div>}
+        />
+      </Route>
 
-  return (
-    <AppLayout>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/vendas" element={<Sales />} />
-        <Route path="/vendas/:id/editar" element={<EditSale />} />
-        <Route path="/pagamentos" element={<Payments />} />
-        <Route path="/produtos" element={<Products />} />
-        <Route path="/auditoria" element={<Audit />} />
-        <Route path="/sobre" element={<About />} />
-        <Route path="/historico" element={<History />} />
-        <Route path="/buscar" element={<Search />} />
-      </Routes>
-    </AppLayout>
+      <Route element={<Authenticated logged={logged} />}>
+        <Route element={<AppLayout />}>
+          <Route
+            index
+            path="/"
+            lazy={() => import("@/pages/Index")}
+            hydrateFallbackElement={<div>carregando...</div>}
+          />
+          <Route
+            path="/vendas"
+            lazy={() => import("@/pages/Sales")}
+            hydrateFallbackElement={<div>carregando...</div>}
+          />
+          <Route
+            path="/vendas/:id/editar"
+            lazy={() => import("@/pages/EditSale")}
+            hydrateFallbackElement={<div>carregando...</div>}
+          />
+          <Route
+            path="/pagamentos"
+            lazy={() => import("@/pages/Payments")}
+            hydrateFallbackElement={<div>carregando...</div>}
+          />
+          <Route
+            path="/produtos"
+            lazy={() => import("@/pages/Products")}
+            hydrateFallbackElement={<div>carregando...</div>}
+          />
+          <Route
+            path="/auditoria"
+            lazy={() => import("@/pages/Audit")}
+            hydrateFallbackElement={<div>carregando...</div>}
+          />
+          <Route
+            path="/sobre"
+            lazy={() => import("@/pages/About")}
+            hydrateFallbackElement={<div>carregando...</div>}
+          />
+          <Route
+            path="/historico"
+            lazy={() => import("@/pages/History")}
+            hydrateFallbackElement={<div>carregando...</div>}
+          />
+          <Route
+            path="/buscar"
+            lazy={() => import("@/pages/Search")}
+            hydrateFallbackElement={<div>carregando...</div>}
+          />
+        </Route>
+      </Route>
+
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Route>,
   );
 }
 
-const App = () => (
-  <LazyMotion features={domMax}>
-    <Toaster />
-    <BrowserRouter>
-      <AuthGate />
-    </BrowserRouter>
-  </LazyMotion>
-);
+const App = () => {
+  const router = createBrowserRouter(AuthGate());
+
+  return (
+    <LazyMotion features={domMax}>
+      <Toaster />
+      <RouterProvider router={router} />
+    </LazyMotion>
+  );
+};
 
 export default App;
