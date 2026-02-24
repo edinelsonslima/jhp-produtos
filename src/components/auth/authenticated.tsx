1536f3940a5065900c839f02a441cff60ef9a3de
Edinelson Lima <edinelsonlima86@gmail.com>
2026-02-24 01:38:55 -0300

    feat: add Authenticated and Unauthenticated components for route protection

---
+++ src/components/auth/authenticated.tsx
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+interface IAuthenticated {
+  logged: boolean;
+}
+
+export function Authenticated({ logged }: IAuthenticated) {
+  return logged ? <Outlet /> : <Navigate to="/login" replace />;
+}
---
+++ src/components/auth/unauthenticated.tsx
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+interface IUnauthenticated {
+  logged: boolean;
+}
+
+export function Unauthenticated({ logged }: IUnauthenticated) {
+  return !logged ? <Outlet /> : <Navigate to="/" replace />;
+}
