4e017e3bd29cebe64ce97ba21fe388d0e72a18a1
Edinelson Lima <edinelsonlima86@gmail.com>
2026-02-26 23:41:42 -0300

    refactor: reorder imports and update export to use defineConfig

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
 import js from "@eslint/js";
 import prettierRecommended from "eslint-plugin-prettier/recommended";
-import globals from "globals";
+import reactCompiler from "eslint-plugin-react-compiler";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import reactCompiler from "eslint-plugin-react-compiler";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: ["dist", "coverage"],
   },
